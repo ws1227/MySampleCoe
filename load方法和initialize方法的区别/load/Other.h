074695a9ada7e396d2fe6ffda58586ec91c15472
wangxiusen <sslxws@163.com>
2017-05-10 19:07:36 +0800

    load方法和initialize方法的区别

---
+++ load方法和initialize方法的区别/load/Other.h
@@ -0,0 +1,17 @@
+//
+//  Normal.h
+//  load
+//
+//  Created by 张星宇 on 16/2/1.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Other : NSObject
+
++ (void)printName;
+- (void)originalFunc;
++ (void)load;
+
+@end
